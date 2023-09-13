108acdec41758a66b277c8086989ea85426a9a18
Abhishekverma13 <137670345+Abhishekverma13@users.noreply.github.com>
2023-09-13 12:05:56 +0530

    Add files via upload

---
+++ arrray.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+    int a = 2;
+    cout<<"Enter the value of the array"<<endl;
+    cin>>a;
+
+    for(int i=0;i<a;i++){
+        cin>>a;
+    }
+
+    return 0;
+
+
+}
+
+
+
+
+
+
+
+
+
+
+/* ARRAY HAS CONSECUTIVE VALUE SAME DATA
+*/
