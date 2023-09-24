d005bccbf286415673e70d88084b5e13112b96d8
Abhishekverma13 <137670345+Abhishekverma13@users.noreply.github.com>
2023-09-24 11:56:32 +0530

    Add files via upload
    using inbuilt swap function
    
    swap(a,b);

---
+++ inbuiltswap.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a = 5, b = 6;
+    cout<<"Before swapping -: "<<endl;
+    cout<<"a = "<<a<<endl;
+    cout<<"b = "<<b<<endl;
+
+    swap(a,b);
+
+    cout<<"After  swapping -: "<<endl;
+    cout<<"a = "<<a<<endl;
+    cout<<"b = "<<b<<endl;
+
+    return 0;
+
+
+
+}
