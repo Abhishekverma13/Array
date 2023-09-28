049097daea96aec19d6ace72a78936f9ace9f2e8
Abhishekverma13 <137670345+Abhishekverma13@users.noreply.github.com>
2023-09-28 12:11:21 +0530

    Add files via upload
    TRY AND CATCH ERROR
    
    try Block:
    
    A try block is used to enclose a section of code where an exception might occur.
    It is the region where you anticipate exceptions and want to handle them.
    Inside the try block, you place code that may generate exceptions, such as division by zero, array index out of bounds, or file I/O errors.
    catch Block:
    
    A catch block follows a try block and is responsible for handling exceptions.
    When an exception occurs within the associated try block, the control flow jumps to the first catch block that matches the type of the thrown exception.
    Multiple catch blocks can be used, each handling a specific type of exception or a base class of exceptions.
    The catch block typically contains code to handle the exception, such as logging an error message or taking corrective actions.

---
+++ trycatch.cpp
@@ -0,0 +1,21 @@
+//try catch exception handing
+#include<iostream>
+using namespace std;
+
+int main(){
+    int numerator, denominator;
+    cin>>numerator;
+    cin>>denominator;
+    try{
+        if(denominator==0)
+        throw ("Divide by zero");
+        numerator/denominator+1;
+        }
+        catch (const char* msg){
+            cout<<msg<<" message"<<endl;}
+
+    
+    float division = numerator/denominator;
+    cout<<" \n divison \n"<<division<<endl;
+    return 0;
+} 
